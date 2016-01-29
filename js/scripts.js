b9e5b91cec2d50af364ca78a8ac4eb7b490e26e0
RobinBKing <samimspop@gmail.com>
2016-01-29 08:59:41 -0800

    add first successful failed test on Order

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 //business logic
+// function Order(deliveryType){
+//   this.pizzas = [];
+//   this.drinks = [];
+//   this.deliveryType = deliveryType;
+// };
+
 
 //interface
 // $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,14 @@
-// describe('template', function() {
-//   it("is true/fals some behavior", function() {
-//     expect(notTriangle(10, 10, 20)).to.equal(true);
-//   });
-// });
+/* global expect */
+describe('Orders', function() {
+  it("creates a new order with given properties", function() {
+    var testOrder = new Order("To go");
+    expect(testOrder.pizzas).to.eql([]);  
+    expect(testOrder.drinks).to.eql([]);        
+    expect(testOrder.deliveryType).to.equal("To go");
+  });
+  // it("adds the fullMovie method to all movies", function() {
+  //    var testOrder = new Order("To go");
+  //   expect(testOrder.orderTotal()).to.equal("$20");
+  // });
+
+});
