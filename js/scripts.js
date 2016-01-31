2b2ea23d39fe85b13dc6d27f5e30e9fb4e1d84e5
RobinBKing <samimspop@gmail.com>
2016-01-30 20:33:58 -0800

    fix bugs on dirnks and pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 //business logic
+function Order(deliveryType){
+  this.pizzas = [];
+  this.drinks = [];
+  this.deliveryType = deliveryType;
+};
+Order.prototype.orderTotal = function() {
+  var orderTotal = 0;
+  for (var i = 0; i < this.pizzas.length; i++){
+    var pizza = this.pizzas[i];
+    orderTotal += pizza.pizzaTotal();
+  };
+  for (var i = 0; i < this.drinks.length; i++){
+    var drink = this.drinks[i];
+    orderTotal += drink.drinkTotal();
+  };
+  return parseFloat(orderTotal);
+};
 function Pizza(size){
   this.size = size;
   this.toppings = [];
@@ -23,50 +40,45 @@ Pizza.prototype.pizzaTotal = function() {
   };
   return parseFloat((this.toppings.length * toppingPrice) + basePrice);
 };
+Pizza.prototype.fullPizza = function() {
+  var allToppings;
+  if (this.toppings.length > 0) {
+    allToppings = this.size + " " + this.toppings[0];
+    for (var i = 1; i < this.toppings.length; i++){
+      allToppings = allToppings + ", " +  this.toppings[i];
+    };allToppings += " pizza $" + this.pizzaTotal().toFixed(2);
+  };
+  return allToppings;
+}
 function Drink(name, size){
   this.name = name;
   this.size = size;
 };
 Drink.prototype.drinkTotal = function() {
-  var price = 0.00;
-  switch(this.drinkSize) {
+  var price = 0;
+  switch(this.size) {
     case "Small":
         price = 0.75;
         break;
     case "Medium":
-        price = 1.00;
+        price = 1;
         break;
     case "Large":
          price = 1.25;
         break;
     default:
-        price = 0;
         break;
    };
    return parseFloat(price);
 };
-function Order(deliveryType){
-  this.pizzas = [];
-  this.drinks = [];
-  this.deliveryType = deliveryType;
-};
-Order.prototype.orderTotal = function() {
-  var orderTotal = 0.00;
-  for (var i = 0; i < this.pizzas.length; i++){
-    var pizza = this.pizzas[i];
-    orderTotal = (orderTotal + pizza.pizzaTotal());
-  };
-  for (var i = 0; i < this.drinks.length; i++){
-    var drink = this.drinks[i];
-    orderTotal = (orderTotal + drink.drinkTotal());
-  };
-  return parseFloat(orderTotal);
+Drink.prototype.fullDrink = function() {
+  return this.size + " " + this.name + " $" + this.drinkTotal().toFixed(2);
 };
 //interface
 $(document).ready(function() {
   var newToppings = ["Pepperoni","Sausage","Beef","Chicken","Onions","Green Peppers","Black Olives","Mushrooms","Tomatos","Garlic","Peppers","Extra Cheese"];
 
-  $("#toppingsCheck").empty;
+  $("#toppingsCheck").empty();
   for(var i=0; i < newToppings.length; i++){
     $("#toppingsCheck").append("<div class='form-group;>" +
                                  "<div class='checkbox'>" +
@@ -74,13 +86,13 @@ $(document).ready(function() {
                                  "</div>" +
                                "</div>");
   };
-
   var newDrinks = ["Cola", "Lemon Lime", "Root Beer", "Lemonade", "Fruit Punch", "Milk"];
-  $("#drinksSelect").empty;
+  $("#drinksSelect").empty();
   $("#drinkSelect").append("<option></option>");
   for(var i=0; i < newDrinks.length; i++){
     $("#drinkSelect").append("<option>" + newDrinks[i] + "</option>");
   };
+  var newOrder = new Order("To go");
   $("form#pizzaForm").submit(function(event) {
     event.preventDefault();
     var newPizzaSize = $("select#pizzaSizeSelect").val();
@@ -91,18 +103,21 @@ $(document).ready(function() {
         newPizza.push(newToppings[i]);
       };
     };
-    var newDrinkselct = $("select#drinkSelect").val();
     var newDrinkSizeSelect = $("select#drinkSizeSelect").val();
-    var newDrink = new Drink(newDrinkselct, newDrinkSizeSelect);
-    var newOrder = new Order("To go");
-    newOrder.pizzas.push(newPizza);
-    newOrder.drinks.push(newDrink);
-
-    $("ul#orderList").empty;
-    for(var i=0; i<newOrder.pizzas.length; i++){
+    var newDrinkselect = $("select#drinkSelect").val();
+    var newDrink = new Drink(newDrinkselect, newDrinkSizeSelect);
+    if(newPizza.size){
+      newOrder.pizzas.push(newPizza);
+      console.log(newOrder.pizzas.length);
+    };
+    if(newDrink.name){
+      newOrder.drinks.push(newDrink);
+      console.log(newOrder.drinks.length);
+    };
+    $("ul#orderList").empty();
+    for(var i = 0; i < newOrder.pizzas.length; i++){
       var orderedPizza = newOrder.pizzas[i];
-      var fullPizza = orderedPizza.size + " Pizza " + orderedPizza.pizzaTotal();
-      $("ul#orderList").append("<li><span class='pizzaOrderdlist'>" + fullPizza +"</span></li>");
+      $("ul#orderList").append("<li><span class='pizzaOrderdlist'>" + orderedPizza.fullPizza() +"</span></li>");
     };
     for(var i=0; i<newOrder.drinks.length; i++){
       var orderedDrinks = newOrder.drinks[i];
@@ -111,7 +126,7 @@ $(document).ready(function() {
         $("ul#orderList").append("<li><span class='drinksOrderdlist'>" + fullDrinks + "</span></li>");
       };
     };
-    $("#orderTotal").text(newOrder.orderTotal());
+    $("#orderTotal").text("$" + newOrder.orderTotal().toFixed(2));
     $("#showOrder").show();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,3 @@
-/* global expect */
 describe('Orders', function() {
   it("creates a new order with given properties", function() {
     var testOrder = new Order("To go");
@@ -19,7 +18,16 @@ describe('Pizza', function() {
   });
   it("adds the pizza total method to Pizza", function() {
     var testPizza = new Pizza("Large");
-    expect(testPizza.pizzaTotal()).to.equal(5);
+    testPizza.toppings.push("peperoni");
+    testPizza.toppings.push("sausage");
+    expect(testPizza.pizzaTotal()).to.equal(11.5);
+  });
+  it("add concatinates all the toppings of the .topping array of Pizza", function() {
+    var testPizza = new Pizza("Large");
+    testPizza.toppings.push("peperoni");
+    testPizza.toppings.push("sausage");
+    testPizza.toppings.push("Extra cheese");
+    expect(testPizza.fullPizza()).to.equal("Large peperoni, sausage, Extra cheese pizza $14.50");
   });
 });
 describe('Drink', function() {
@@ -30,6 +38,10 @@ describe('Drink', function() {
   });
   it("adds the Drink total method to Drink", function() {
     var testDrink = new Drink("Root Beer", "Medium");
-    expect(testDrink.drinkTotal()).to.equal(0);
+    expect(testDrink.drinkTotal()).to.equal(1);
+  });
+  it("add concatinates the dring and size of Drink", function() {
+    var testDrink = new Drink("Root Beer", "Medium");
+    expect(testDrink.fullDrink()).to.equal("Medium Root Beer $1.00");
   });
 });
